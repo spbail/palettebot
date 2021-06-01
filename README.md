af864bff0aa8711ca2fa382f52d2898dfb27f1c0
Sam Bail <samanthapbail@gmail.com>
2021-06-01 01:24:56 -0400

    Working version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # palettebot
 A color palette bot experiment, just because!
+
+Posts to twitter.com/colpalettebot
--- bot.js
+++ bot.js
@@ -13,23 +13,97 @@ var config = {
 const T = new twit(config)
 
 
-function postTweet() {
+function getPaletteImage() {
+    const fs = require('fs')
+    const { createCanvas } = require('canvas')
 
-    T.post('statuses/update', { status: 'hello world! beep beep :)' }, function(err, data, response) {
-        console.log(data)
+    const canvWidth = 400
+    const canvHeight = 230
+
+    const canvas = createCanvas(canvWidth, canvHeight)
+    const ctx = canvas.getContext('2d')
+
+    var width = 72;
+    var height = 190;
+    var offset = 20;
+
+    var [colors, description] = getRandomPalette();
+
+    colors.forEach(function(col, i) {
+        ctx.fillStyle = col;
+        ctx.fillRect((width * i) + offset, offset, width, height);
     })
 
+    const ts = new Date().toISOString().replace(/:/g, '');
+    var fileName = './img/img_' + ts + '.png';
+    console.log('Generated image ' + fileName);
+    const buffer = canvas.toBuffer('image/png');
+    fs.writeFileSync(fileName, buffer);
+    return [fileName, description]
+
+}
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+}
+
+function getRandomPalette() {
+    // Using https://github.com/c0bra/color-scheme-js
+    var colorScheme = require('color-scheme');
+    var scheme = new colorScheme;
+
+    // Generating a random hue and random variation for the color scheme
+    var randomHue = randomInteger(0, 359);
+    console.log('Random hue: ' + randomHue);
+
+    var variations = Object.keys(colorScheme.PRESETS)
+    var randomVariation = shuffleArray(variations)[0]
+    console.log('Random variation: ' + randomVariation)
+
+    scheme.from_hue(randomHue) // Start the scheme 
+        .scheme('triade') // Use the 'triade' scheme, that is, colors
+        // selected from 3 points equidistant around
+        // the color wheel.
+        .variation(randomVariation); // Use the 'soft' color variation
+
+    var allColors = scheme.colors();
+    // Adding the # prefix
+    allColors.forEach(function(col, index, arr) {
+        arr[index] = '#' + col;
+    });
+    console.log('All colors:')
+    console.log(allColors)
+
+    // Randomly selecting 5 colors to draw on the canvas
+    var selectedColors = shuffleArray(allColors).splice(0, 5)
+
+    console.log('Selected colors:')
+    console.log(selectedColors)
+
+    var description = '\n\ncolor-scheme.js parameters used: \n- hue: ' +
+        randomHue + '\n- variation: ' + randomVariation + '\n' +
+        '- colors: ' + selectedColors.join(', ')
+    return [selectedColors, description]
 }
 
 function postTweetWithImage(statusText, imgPath) {
     //
-    // post a tweet with media - example from the Twit docs
+    // Post a tweet with media - example from the Twit docs
     //
+
     var b64content = fs.readFileSync(imgPath, { encoding: 'base64' })
 
-    // first we must post the media to Twitter
+    // First we must post the media to Twitter
     T.post('media/upload', { media_data: b64content }, function(err, data, response) {
-        // now we can assign alt text to the media, for use by screen readers and
+        // Now we can assign alt text to the media, for use by screen readers and
         // other text-based presentations and interpreters
         var mediaIdStr = data.media_id_string
         var altText = "A color palette"
@@ -37,7 +111,7 @@ function postTweetWithImage(statusText, imgPath) {
 
         T.post('media/metadata/create', meta_params, function(err, data, response) {
             if (!err) {
-                // now we can reference the media and post a tweet (media will attach to the tweet)
+                // Now we can reference the media and post a tweet (media will attach to the tweet)
                 var params = { status: statusText, media_ids: [mediaIdStr] }
 
                 T.post('statuses/update', params, function(err, data, response) {
@@ -50,5 +124,9 @@ function postTweetWithImage(statusText, imgPath) {
 
 
 // Post, then exit - this is run every interval by the Heroku scheduler
-postTweetWithImage('Beep beep, here\'s a pretty color palette for you!', './palette1.jpg');
+[imgPath, description] = getPaletteImage();
+postTweetWithImage('Beep beep, here\'s a pretty color palette for you! ' +
+    description + '\n\nHave a lovely day!' +
+    '\n\n#twitterbot #colorpalette #colors #palette #prettycolors', imgPath
+);
 process.exitCode = 0;
